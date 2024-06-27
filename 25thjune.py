9193a498b4613b33c05b7a6412e1190c16df75da
farazhafeez26 <faraz.hafiz@gmail.com>
2024-06-27 09:50:46 +0200

    27th June

--- 25thjune.py
+++ 25thjune.py
@@ -17,7 +17,7 @@ firstandlastlastequal(enter_text) '''
 Write a function named extract_domain that takes a single argument: a string representing a dot-com URL like "https://www.example.com".
 The function should return only the domain name, in this case, "example".
 Assume the provided URL will always start with "https://www." and will always contain a domain name followed by ".com".
-You don't need to handle input or call the function at all.'''
+You don't need to handle input or call the function at all.
 
 def extract_domain(url): 
     return url[4:-4] 
@@ -26,3 +26,31 @@ modified_url =input("Enter the URL:")
 result=extract_domain(modified_url)
 print(result)
 
+'''
+
+#code challenge - Conditional slicing
+'''Code Challenge · Conditional Slicing
+Due to a mistake at the local school, some student names begin with an underscore (“_”).
+Create a function named clean_name that takes one argument: the student's name (str).
+If the name starts with an underscore, the function should return the name without it. 
+Otherwise, it should return the name unchanged.
+You don't need to handle input or call the function at all.'''
+
+def new_name(text): 
+    text = text.replace("_", " ")
+    print(f"new sentence {text}")
+
+sentence=input("Enter a sentence: ")
+
+new_name(sentence)
+
+
+
+
+
+'''Code Challenge · Dollar to Yuan
+Define a function called convert_to_yuan that accepts one argument: a string representing a money amount in dollars, formatted like "1542$".
+The function should return the corresponding amount in Yuan as an int.
+For this exercise, use a conversion rate of 1 Dollar = 7.15 Yuan.
+There's no need to handle user input or invoke the function.
+Hint: To be able to perform arithmetic operations, you need to use the int function on the string that holds the amount of money.'''
