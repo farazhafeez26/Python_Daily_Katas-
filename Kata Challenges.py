6ff00433bad337e473e2664bb195a8e70f5cf9f8
farazhafeez26 <faraz.hafiz@gmail.com>
2024-07-04 11:58:43 +0200

    4thjuly

--- Kata Challenges.py
+++ Kata Challenges.py
@@ -1,3 +1,4 @@
+
 '''Write a function, first_and_last_equal, that takes a string as input and prints "Yes" if the first and last characters of the string are the same, or "No" otherwise. For instance, first_and_last_equal("everyone") should print “Yes!
 
 def firstandlastlastequal(text):
@@ -8,12 +9,12 @@ def firstandlastlastequal(text):
     
 enter_text= input("Enter your text:")
 
-firstandlastlastequal(enter_text) '''
+firstandlastlastequal(enter_text) 
 
 
 #code challenge  -Domain extractor
 
-'''Code Challenge · Domain extractor
+Code Challenge · Domain extractor
 Write a function named extract_domain that takes a single argument: a string representing a dot-com URL like "https://www.example.com".
 The function should return only the domain name, in this case, "example".
 Assume the provided URL will always start with "https://www." and will always contain a domain name followed by ".com".
@@ -26,16 +27,17 @@ modified_url =input("Enter the URL:")
 result=extract_domain(modified_url)
 print(result)
 
-'''
+
 
 #code challenge - Conditional slicing
-'''Code Challenge · Conditional Slicing
+
+Code Challenge · Conditional Slicing
 Due to a mistake at the local school, some student names begin with an underscore (“_”).
 Create a function named clean_name that takes one argument: the student's name (str).
 If the name starts with an underscore, the function should return the name without it. 
 Otherwise, it should return the name unchanged.
-You don't need to handle input or call the function at all.'''
-'''
+
+You don't need to handle input or call the function at all.
 def new_name(text): 
     text = text.replace("_", " ")
     print(f"new sentence {text}")
@@ -43,8 +45,8 @@ def new_name(text):
 sentence=input("Enter a sentence: ")
 
 new_name(sentence)
-'''
-'''
+
+
 def new_list(text): 
     name_list=text.split(",")
     print(name_list)
@@ -64,20 +66,19 @@ input_text= input("Enter the name: ")
 
 new_list(input_text)
 
-'''    
+    
 
 
 
 
-'''Code Challenge · Dollar to Yuan
+Code Challenge · Dollar to Yuan
 Define a function called convert_to_yuan that accepts one argument: a string representing a money amount in dollars, formatted like "1542$".
 The function should return the corresponding amount in Yuan as an int.
 For this exercise, use a conversion rate of 1 Dollar = 7.15 Yuan.
 There's no need to handle user input or invoke the function.
-Hint: To be able to perform arithmetic operations, you need to use the int function on the string that holds the amount of money.'''
+Hint: To be able to perform arithmetic operations, you need to use the int function on the string that holds the amount of money.
 
 
-'''
 
 Task
 
@@ -87,9 +88,8 @@ The highest or lowest element respectively is a single element at each edge, eve
 
 Mind the input validation.
 
-'''
 
-'''
+
 def sum_array(arr):
     if not arr or len(arr) < 3:
         return 0
@@ -104,9 +104,9 @@ def sum_array(arr):
 
 print(sum_array([6, 2, 1, -3, 10]))
 
-'''
 
-'''
+
+
 Grade book
 
 Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
@@ -118,8 +118,6 @@ Numerical Score	Letter Grade
 60 <= score < 70	'D'
 0 <= score < 60	'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
-
-'''
 
 def get_grade(s1, s2, s3):
     # Code here
@@ -144,12 +142,14 @@ def get_grade(s1, s2, s3):
     # Action - i removed >=100
 
     # other ways to do it:
-    '''def get_grade(s1, s2, s3):
+
+def get_grade(s1, s2, s3):
     score =(s1+s2+s3)/3
-    return 'A' if score >= 90 else 'B' if score >= 80 else 'C' if score >= 70 else 'D' if score >= 60 else 'F'''
+    return 'A' if score >= 90 else 'B' if score >= 80 else 'C' if score >= 70 else 'D' if score >= 60 else 'F' 
 
 
-'''
+
+------------------------------------
 Can you find the needle in the haystack?
 
 Write a function findNeedle() that takes an array full of junk but containing one "needle"
@@ -162,9 +162,51 @@ Example(Input --> Output)
 
 ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
 
-'''
 
 def find_needle(haystack):
     if "needle" in haystack: 
         needle_position=haystack.index("needle")
-        return f"found the needle at position {needle_position}"
+        return f"found the needle at position {needle_position}"  
+
+
+
+
+-------------------------------------
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+def count_positives_sum_negatives(arr):
+    list=[]
+    list2=[]
+    
+    if not arr: 
+        return []
+    for i in arr: 
+        if i <0:
+            list.append(i)
+        elif i > 0: 
+            list2.append(i)
+            
+        
+
+    return [len(list2), sum(list)]
+
+count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+
+another way of doing it
+def count_positives_sum_negatives(arr):
+    pos = sum(1 for x in arr if x > 0)
+    neg = sum(x for x in arr if x < 0)
+    return [pos, neg] if len(arr) else []
+
+#lesson learned - look for input validation line number 179
+'''
+
